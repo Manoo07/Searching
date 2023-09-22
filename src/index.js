cfbb01d21f369f841c70dc49452dda53e4ff477b
Manoo07 <manoharboinapalli1196@gmail.com>
2023-09-22 16:39:10 +0530

    This is first commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const express=require('express');
+require('dotenv').config();
+const { PORT } = require('./config/serverConfig');
+const app=express();
+
+const setUpAndStartServer = async ()=>{
+    //  create the express object
+    const app=express();
+    app.listen(PORT,()=>{
+        console.log(`Server started at ${PORT} Port`);
+    })
+}
+setUpAndStartServer();
+const bodyparser=require('body-parser');
