bba87389c7bf4b0a404939d7c3890714476ce027
Manoo07 <manoharboinapalli1196@gmail.com>
2023-09-22 22:54:49 +0530

    some sequelize files are added

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const setUpAndStartServer = async ()=>{
     //  create the express object
     const app=express();
     app.use(bodyparser.json());
-    app.use(urlencoded({extended:true}));
+    app.use(urlencoded({extended:true})); 
     app.listen(PORT,()=>{
         console.log(`Server started at ${PORT} Port`);
     })
