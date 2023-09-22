9d8c15290ee6bfea9cb361d0c6ee6e4663c25844
Manoo07 <manoharboinapalli1196@gmail.com>
2023-09-22 23:42:11 +0530

    Completed a basic sequelize DB setup for creating a DB

---
+++ src/guide.js
@@ -0,0 +1,14 @@
+/*
+
+1.From DataBase creation 
+- we are using sql dataBase so, using SQL like mysql,postgre is difficult
+  so,we use some drivers and some supporters these are some npm packages and modules
+  for any further details refer documentation of corresponding package 
+-> npm i sequelize
+-> npm i sequelize-cli
+-> npm i mysql2
+- after doing it , for using sequelize we need to do npx sequelize init
+- bring the new folders into the src folder and cd to src ,then run the command 
+- npx sequelize db:create
+
+*/
