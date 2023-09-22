a9c840460be7cbff330c332600243ef1a48f57dd
Manoo07 <manoharboinapalli1196@gmail.com>
2023-09-22 16:57:53 +0530

    Some changes are been done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express=require('express');
 require('dotenv').config();
-const { PORT } = require('./config/serverConfig');
+const {PORT}=require('./config/serverConfig');
 const app=express();
 
 const setUpAndStartServer = async ()=>{
