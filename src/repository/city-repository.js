2f3f635d5ceaaa362d7685ef9eccacfb5b790f4f
Manoo07 <manoharboinapalli1196@gmail.com>
2023-09-24 11:30:52 +0530

    Added all cities to get all cities

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -1,4 +1,4 @@
-// const { Op } = require('sequelize'); 
+const { Op } = require('sequelize'); 
 const {City}=require('../models/index');
 
 class CityRepository{
@@ -50,8 +50,18 @@ class CityRepository{
             console.log("Something is wrong in updating city : " ,error);
         }
     }
-    async getAllCities(){
+    async getAllCities(filter){
         try {
+            if(filter.name){
+                const cities = await City.findAll({
+                    where:{
+                        name: {
+                            [Op.startsWith]: filter.name,
+                        }
+                    }
+                });
+                return cities;
+            }
             const cities = await City.findAll();
             return cities;
         } catch (error) {
